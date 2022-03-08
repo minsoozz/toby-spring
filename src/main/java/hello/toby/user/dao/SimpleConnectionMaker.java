383ba515249a496b557cb26f3d9b6d4624bab1d9
minsoozz <minsoo1604@naver.com>
2022-03-08 20:30:07 +0900

    DAO의 확장

---
+++ src/main/java/hello/toby/user/dao/SimpleConnectionMaker.java
@@ -0,0 +1,16 @@
+package hello.toby.user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SimpleConnectionMaker {
+
+  public Connection makeNewConnection() throws ClassNotFoundException, SQLException {
+    Class.forName("com.mysql.jdbc.Driver");
+    Connection c = DriverManager.getConnection(
+        "jdbc:mysql://localhost/springbook", "root", "1234");
+    return c;
+  }
+
+}
