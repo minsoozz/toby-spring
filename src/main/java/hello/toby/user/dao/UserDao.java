485cb489e20a758e903aa74b1b20d947c7d1784a
minsoozz <minsoo1604@naver.com>
2022-03-10 17:41:20 +0900

    싱글톤 레지스트리와 오브젝트 스코프

--- src/main/java/hello/toby/user/dao/UserDao.java
+++ src/main/java/hello/toby/user/dao/UserDao.java
@@ -11,6 +11,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class UserDao {
 
   private ConnectionMaker connectionMaker;
+  private Connection c;
+  private User user;
 
   public UserDao(ConnectionMaker connectionMaker) {
     this.connectionMaker = connectionMaker;
@@ -34,7 +36,7 @@ public class UserDao {
 
   public User get(String id) throws ClassNotFoundException, SQLException {
     Class.forName("com.mysql.jdbc.Driver");
-    Connection c = connectionMaker.makeConnection();
+    this.c = connectionMaker.makeConnection();
 
     PreparedStatement ps = c.prepareStatement(
         "select * from users where id = ?");
@@ -44,9 +46,9 @@ public class UserDao {
     ResultSet rs = ps.executeQuery();
     rs.next();
     User user = new User();
-    user.setId(rs.getString("id"));
-    user.setName(rs.getString("name"));
-    user.setPassword(rs.getString("password"));
+    this.user.setId(rs.getString("id"));
+    this.user.setName(rs.getString("name"));
+    this.user.setPassword(rs.getString("password"));
 
     rs.close();
     ps.close();
@@ -57,21 +59,18 @@ public class UserDao {
 
   public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
+    DaoFactory factory = new DaoFactory();
+    UserDao dao1 = factory.userDao();
+    UserDao dao2 = factory.userDao();
+
+    System.out.println("dao1 = " + dao1);
+    System.out.println("dao2 = " + dao2);
+
     ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-    UserDao dao = context.getBean("userDao", UserDao.class);
-    User user = new User();
-    user.setId("minsoo");
-    user.setName("김민수");
-    user.setPassword("hello");
+    UserDao dao3 = context.getBean("userDao", UserDao.class);
+    UserDao dao4 = context.getBean("userDao", UserDao.class);
 
-    dao.add(user);
-
-    System.out.println("user.getId() =" + user.getId());
-
-    User user2 = dao.get(user.getId());
-
-    System.out.println("user2.getName() = " + user2.getName());
-    System.out.println("user2.getPassword() = " + user2.getPassword());
-    System.out.println("user2.getId() = " + user2.getId());
+    System.out.println("dao3 = " + dao3);
+    System.out.println("dao4 = " + dao4);
   }
 }
