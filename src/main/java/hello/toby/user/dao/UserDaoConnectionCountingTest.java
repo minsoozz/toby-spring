9af3f5b78c15b5cbe86585fa0036bacf26f9257a
minsoozz <minsoo1604@naver.com>
2022-03-10 19:37:23 +0900

    의존관계 주입(DI)

---
+++ src/main/java/hello/toby/user/dao/UserDaoConnectionCountingTest.java
@@ -0,0 +1,14 @@
+package hello.toby.user.dao;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class UserDaoConnectionCountingTest {
+
+  public static void main(String[] args) {
+    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(CountingDaoFactory.class);
+    UserDao dao = context.getBean("userDao", UserDao.class);
+
+    CountingConnectionMaker ccm = context.getBean("connectionMaker", CountingConnectionMaker.class);
+    System.out.println("ccm.counter = " + ccm.getCounter());
+  }
+}
